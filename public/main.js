7330cb2c10ddc966f2bb1141a0cfae8d49864545
JoabChua <chuaweeliang91@gmail.com>
2019-04-29 23:44:30 +0800

    basic html and css of the card and columns are done. json server is set up with localhost:3000. project view is setup with express running on localhost:8080

---
+++ public/main.js
@@ -0,0 +1,26 @@
+var http = new XMLHttpRequest();
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function httpPost(url, data, callback) {
+    http.open('POST', url, true);
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            alert(http.responseText);
+        }
+    }
+    // data = JSON.stringify(data);
+    http.send(data);
+}
