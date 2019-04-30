6f43fdf071dc8e47391a2068e7351fcd28d64b91
JoabChua <chuaweeliang91@gmail.com>
2019-04-30 16:06:08 +0800

    edit column and delete column are working well

--- public/main.js
+++ public/main.js
@@ -35,6 +35,19 @@ function httpPost(url, data, callback) {
     xhr.send(data);
 }
 
+function httpPut(url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function (responseText) {
+        if (this.readyState === 4) {
+            callback(JSON.parse(this.responseText));
+        }
+    });
+
+    xhr.open("PUT", url);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+}
+
 function httpDelete(url, callback) {
     // var xhr = new XMLHttpRequest();
     // xhr.addEventListener("readystatechange", function () {
@@ -73,29 +86,29 @@ function toggleInput() {
     }
 }
 
-function openModal(){
-    var modal = document.getElementById('myModal');
+// function openModal(){
+//     var modal = document.getElementById('myModal');
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
+//     // Get the button that opens the modal
+//     var btn = document.getElementById("myBtn");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+//     // Get the <span> element that closes the modal
+//     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+//     // When the user clicks on the button, open the modal 
+//     btn.onclick = function() {
+//         modal.style.display = "block";
+//     }
 
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+//     // When the user clicks on <span> (x), close the modal
+//     span.onclick = function() {
+//         modal.style.display = "none";
+//     }
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-}
+//     // When the user clicks anywhere outside of the modal, close it
+//     window.onclick = function(event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     }
+// }
