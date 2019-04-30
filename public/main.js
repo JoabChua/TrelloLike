6f43e4a6f2778ea9f9b39feaa842e8f4cc0cc89b
JoabChua <chuaweeliang91@gmail.com>
2019-04-30 18:48:34 +0800

    self implemented drag and drop functionality working well

--- public/main.js
+++ public/main.js
@@ -12,14 +12,6 @@ function httpGet(url, callback)
         }
     }
     http.send(null);
-
-    // var xmlHttp = new XMLHttpRequest();
-    // xmlHttp.onreadystatechange = function() { 
-    //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-    //         callback(xmlHttp.responseText);
-    // }
-    // xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    // xmlHttp.send(null);
 }
 
 function httpPost(url, data, callback) {
@@ -48,23 +40,27 @@ function httpPut(url, data, callback) {
     xhr.send(data);
 }
 
+function httpPatch(url, data, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function (responseText) {
+        if (this.readyState === 4) {
+            callback(JSON.parse(this.responseText));
+        }
+    });
+
+    xhr.open("PATCH", url);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+}
+
 function httpDelete(url, callback) {
-    // var xhr = new XMLHttpRequest();
-    // xhr.addEventListener("readystatechange", function () {
-    //     if (this.readyState === 4) {
-    //         callback();
-    //     }
-    // });
-    // xhr.open("DELETE", url);
-    // xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    // xhr.send(null);
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() { 
+        if (xhr.readyState == 4 && xhr.status == 200)
+            callback(xhr.responseText);
     }
-    xmlHttp.open("DELETE", url, true); // true for asynchronous 
-    xmlHttp.send(null);
+    xhr.open("DELETE", url, true); // true for asynchronous 
+    xhr.send(null);
 }
 
 function alertSize() {
@@ -86,29 +82,120 @@ function toggleInput() {
     }
 }
 
-// function openModal(){
-//     var modal = document.getElementById('myModal');
+function dropdownContent(){
+    var acc = document.getElementsByClassName("card-title");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+        panel.style.display = "none";
+        } else {
+        panel.style.display = "block";
+        }
+    });
+    }
+}
 
-//     // Get the button that opens the modal
-//     var btn = document.getElementById("myBtn");
+function getJSONCol() {
+    httpGet('http://localhost:3000/columns', function(response) {
+        col = JSON.parse(response);
+        getJSONCard();
+    });
+}
 
-//     // Get the <span> element that closes the modal
-//     var span = document.getElementsByClassName("close")[0];
+function getJSONCard() {
+    httpGet('http://localhost:3000/cards', function(response) {
+        cards = JSON.parse(response);
+        setColCards();
+    });
+}
 
-//     // When the user clicks on the button, open the modal 
-//     btn.onclick = function() {
-//         modal.style.display = "block";
-//     }
+function deleteCard(ev){
+    httpDelete('http://localhost:3000/cards/' + ev, function(response) {
+        getJSONCol();
+    });
+}
 
-//     // When the user clicks on <span> (x), close the modal
-//     span.onclick = function() {
-//         modal.style.display = "none";
-//     }
+function editCard(ev){
+    console.log(ev);
+    var title = prompt("Edit Card Title",cards[ev-1].title);
+    var description = prompt("Edit Card Description:",cards[ev-1].description);
+    if (title == null || description == null){
+        // nth happens
+    } else {
+        let numb = cards[ev-1].columnId;
+        var data = `title=${title}&description=${description}&columnId=${numb}`;
+        httpPut('http://localhost:3000/cards/' + ev, data, function(response){
+            getJSONCol();
+        })
+    }
+}
 
-//     // When the user clicks anywhere outside of the modal, close it
-//     window.onclick = function(event) {
-//         if (event.target == modal) {
-//             modal.style.display = "none";
-//         }
-//     }
-// }
+function newCard(ev){
+    var title = prompt("New Card Title","");
+    var description = prompt("New Card Description:","");
+    let checkDuplicateTitle = false;
+    for (var i = 0 ; i < cards.length; i++){
+        if (title == cards[i].title){
+            checkDuplicateTitle = true;
+        }
+    }
+    if (title == null || description == null){
+        alert("Card creation cancelled!");
+    } else if (checkDuplicateTitle) {
+        alert("Error! Duplicate Card Title");
+    } else {
+        let numb = ev.path[2].children[1].id;
+        // console.log(ev);
+        var data = `title=${title}&description=${description}&columnId=${numb}`;
+        // console.log(data);
+        httpPost('http://localhost:3000/cards', data, function(response){
+            getJSONCol();
+        })
+    }
+}
+
+function newList(ev){
+    // console.log(ev);
+    if (ev.keyCode === 13 || ev.type == "click") {
+        var title = document.getElementById('newlist').value;
+        let checkDuplicateTitle = false;
+        for( var i = 0; i < col.length; i++){
+            if(title == col[i].title){
+                checkDuplicateTitle = true;
+            }
+        }
+        if (title == "") {
+            alert('Title cannot be empty string!');
+        } else if (checkDuplicateTitle) {
+            alert('Error! Duplicate Column title!');
+        } else {
+            var data = `title=${title}`;
+            httpPost('http://localhost:3000/columns', data, function(response){
+                getJSONCol();
+            })
+        }
+    }
+}
+
+function deleteCol(ev){
+    httpDelete('http://localhost:3000/columns/' + ev, function(response) {
+        getJSONCol();
+    });
+}
+
+function editCol(ev){
+    var title = prompt("Edit Column Name:",col[ev-1].title);
+    if (title == null){
+        // nth happens
+    } else {
+        let numb = ev
+        var data = `title=${title}`;
+        // console.log(data);
+        httpPut('http://localhost:3000/columns/' + ev, data, function(response){
+            getJSONCol();
+        })
+    }
+}
+
