c43c5b5eb05d0124f2d458f62f09e5f1bae98629
JoabChua <chuaweeliang91@gmail.com>
2019-04-30 12:26:34 +0800

    fetch col and card info from db.json and display in view.

--- public/main.js
+++ public/main.js
@@ -1,14 +1,25 @@
 var http = new XMLHttpRequest();
 
-function httpGetAsync(theUrl, callback)
+function httpGet(url, callback)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
+    http.open('GET', url, true);
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if(http.readyState == 4 && http.status == 200) {
+            callback(http.responseText);
+        }
     }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
-    xmlHttp.send(null);
+    http.send(null);
+
+    // var xmlHttp = new XMLHttpRequest();
+    // xmlHttp.onreadystatechange = function() { 
+    //     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+    //         callback(xmlHttp.responseText);
+    // }
+    // xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    // xmlHttp.send(null);
 }
 
 function httpPost(url, data, callback) {
@@ -18,9 +29,28 @@ function httpPost(url, data, callback) {
 
     http.onreadystatechange = function() {//Call a function when the state changes.
         if(http.readyState == 4 && http.status == 200) {
-            alert(http.responseText);
+            callback(http.responseText);
         }
     }
     // data = JSON.stringify(data);
     http.send(data);
+}
+
+function alertSize() {
+    var myWidth = 0, myHeight = 0;
+    if( typeof( window.innerWidth ) == 'number' ) {
+        //Non-IE
+        myWidth = window.innerWidth;
+        myHeight = window.innerHeight;
+    }
+        document.getElementsByClassName("canvas")[0].style.height = myHeight - 80 + 'px';
+}
+
+function toggleInput() {
+    var x = document.getElementById("togggleList");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+    } else {
+        x.style.display = "none";
+    }
 }
